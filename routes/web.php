7ac1115a00c10835945d0571129910cdf859d433
root <root@localhost.localdomain>
2020-04-07 08:42:06 +0000

    修改密码

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+   Route::get('/password','Password\PasswordController@password');
+  Route::post('/password/update','Password\PasswordController@update');
